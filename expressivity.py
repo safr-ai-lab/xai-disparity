ee4b36904cc3db0b270e1ec347e4bda3afa94693
FishmanL <leor@fractal.co>
2022-01-28 12:34:07 -0500

    fixing a bunch of issues

--- expressivity.py
+++ expressivity.py
@@ -3,7 +3,6 @@ import numpy as np
 from typing import Any, Callable, NewType
 from random import uniform
 
-
 CostFuncGenType = NewType("CostFuncGenType", Callable[[np.ndarray, int], Callable[[np.ndarray], float]])
 
 
@@ -30,29 +29,36 @@ def fit_costs_dataset(dataset: np.ndarray, feature_num: int,
     return predictions, cost
 
 
+def total_cost(dataset, feature_num, cost_func_gen: CostFuncGenType):
+    cost_func = cost_func_gen(dataset, feature_num)
+    return sum(cost_func(row) for row in dataset)
+
+
 def extremize_costs_dataset(dataset: np.ndarray, cost_func_gen: CostFuncGenType):
     max_costs = np.array(
-        [(feature_num, fit_costs_dataset(dataset, feature_num, cost_func_gen, minimize=False)) for feature_num in range(dataset.shape[1]-1)])
+        [(feature_num, fit_costs_dataset(dataset, feature_num, cost_func_gen, minimize=False),
+          total_cost(dataset, feature_num, cost_func_gen)) for feature_num in range(dataset.shape[1] - 1)])
     min_costs = np.array(
-        [(feature_num, fit_costs_dataset(dataset, feature_num, cost_func_gen, minimize=True)) for feature_num in range(dataset.shape[1]-1)])
-    feature_num, (max_predictions, max_cost) = sorted(max_costs, key=lambda x: x[1][1], reverse=True)[0]
-    feature_num, (min_predictions, min_cost) = sorted(min_costs, key=lambda x: x[1][1])[0]
+        [(feature_num, fit_costs_dataset(dataset, feature_num, cost_func_gen, minimize=True),
+          total_cost(dataset, feature_num, cost_func_gen)) for feature_num in range(dataset.shape[1] - 1)])
+    feature_num, (max_predictions, max_cost), total = sorted(max_costs, key=lambda x: (x[1][1] - x[2]), reverse=True)[0]
+    feature_num, (min_predictions, min_cost), total= sorted(min_costs, key=lambda x: (x[1][1] - x[2]))[0]
     if abs(max_cost) >= abs(min_cost):
-        return feature_num, max_predictions, max_cost
-    return feature_num, min_predictions, min_cost
+        return feature_num, max_predictions, max_cost-total, total
+    return feature_num, min_predictions, min_cost-total, total
 
 
 def means_cost_func(dataset: np.ndarray, feature_num: int) -> Callable[[np.ndarray], float]:
     probability_f_i = dataset[dataset[..., feature_num] == 1].shape[0] / dataset.shape[0]
-    return lambda row: 2 * (row[feature_num] * probability_f_i - probability_f_i) * row[-1]
+    return lambda row: (row[feature_num] - probability_f_i) * row[-1] / (probability_f_i * (1 - probability_f_i))
 
 
 means_extremize = lambda dataset: extremize_costs_dataset(dataset, means_cost_func)
 
 
+def coinflip(threshold: float) -> int:
+    return int(uniform(0, 1) < threshold)
 
-def coinflip(threshold:float) -> int:
-    return int(uniform(0, 1)<threshold)
 
 num_list = 4000
 
@@ -60,9 +66,9 @@ poor_list = []
 for x in range(num_list):
     poor_elem = [0, coinflip(0)]
     if poor_elem[1] == 1:
-        poor_elem.append(coinflip(0.8))
+        poor_elem.append(coinflip(0.6))
     else:
-        poor_elem.append(coinflip(0.4))
+        poor_elem.append(coinflip(0.6))
     poor_list.append(poor_elem)
 
 rich_list = []
@@ -78,10 +84,9 @@ poor_list.extend(rich_list)
 
 poor_list = np.array(poor_list)
 
-feature_num, predictions, cost = means_extremize(poor_list)
+feature_num, predictions, cost, total = means_extremize(poor_list)
 
 predictions = poor_list[np.array(predictions)]
 print(set((elem[0], elem[1], elem[2]) for elem in predictions))
 
-print(feature_num, cost)
-
+print(feature_num, cost, total)
