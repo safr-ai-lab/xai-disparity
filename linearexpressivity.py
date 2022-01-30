4b05b123e5f194d97d0cbc058e1f24aef5f00dad
FishmanL <leor@fractal.co>
2022-01-30 11:24:27 -0500

    moving basis outside loss_fn

--- linearexpressivity.py
+++ linearexpressivity.py
@@ -5,13 +5,13 @@ from torch.nn.functional import one_hot
 x = torch.Tensor()
 y = torch.Tensor()
 feature_num = 0
+basis = one_hot(torch.Tensor([feature_num]))
 
 
 def loss_fn(params):
     one_d = sigmoid(x * params)
     x_t = torch.t(x)
     diag = torch.diag(one_d)
-    basis = one_hot(torch.Tensor(feature_num))
     return basis*torch.inverse(x*x_t*diag)*(x_t*diag*y)
 
 
