b10e89be2ba2f65507e087902680e3468e2a244a
FishmanL <leor@fractal.co>
2022-01-30 11:26:23 -0500

    making loss fn into factory

--- linearexpressivity.py
+++ linearexpressivity.py
@@ -5,14 +5,15 @@ from torch.nn.functional import one_hot
 x = torch.Tensor()
 y = torch.Tensor()
 feature_num = 0
-basis = one_hot(torch.Tensor([feature_num]))
 
 
-def loss_fn(params):
-    one_d = sigmoid(x * params)
-    x_t = torch.t(x)
-    diag = torch.diag(one_d)
-    return basis*torch.inverse(x*x_t*diag)*(x_t*diag*y)
+def loss_fn_generator(feature_num):
+    basis = one_hot(torch.Tensor([feature_num]))
 
+    def loss_fn(params):
+        one_d = sigmoid(x * params)
+        x_t = torch.t(x)
+        diag = torch.diag(one_d)
+        return basis * torch.inverse(x * x_t * diag) * (x_t * diag * y)
 
-
+    return loss_fn
