83b004f2a7e6af461801973d76f7855ddd3c74f4
FishmanL <leor@fractal.co>
2022-01-31 11:56:21 -0500

    compas!

--- linearexpressivity.py
+++ linearexpressivity.py
@@ -1,17 +1,24 @@
 import torch
 from torch.special import expit as sigmoid
 from torch.optim import Adam
+from aif360.datasets import CompasDataset
+
+
+
+compas_df = CompasDataset().convert_to_dataframe()[0]
 
 torch.cuda.set_device('cuda:0')
-x = torch.randn(30,30, requires_grad=True)
-y = torch.randn(30,1, requires_grad=True)
+
+y = torch.tensor(compas_df["two_year_recid"].values).float().cuda()
+x = torch.tensor(compas_df.drop("two_year_recid", axis=1).values).float().cuda()
+
 
 
 
 def loss_fn_generator(feature_num, minimize = False):
     basis_list = [[0. for _ in range(x.shape[1])]]
     basis_list[0][feature_num] = 1.
-    basis = torch.tensor(basis_list, requires_grad=True)
+    basis = torch.tensor(basis_list, requires_grad=True).cuda()
     factor = -1
     if minimize:
         factor = 1
@@ -27,44 +34,44 @@ def loss_fn_generator(feature_num, minimize = False):
 
 
 def train_and_return(feature_num, initial_value):
-    params_min = torch.randn(x.shape[1], requires_grad=True)
+    params_min = torch.randn(x.shape[1], requires_grad=True,device="cuda")
     optim = Adam(params=[params_min], lr=0.05)
     iters = 0
     curr_error = 10000
     loss_min = loss_fn_generator(feature_num, minimize=True)
-    while (curr_error > 1) and (iters < 1000):
+    while (curr_error > 1) and (iters < 100):
         optim.zero_grad()
         loss_res = loss_min(params_min)
         loss_res.backward()
         optim.step()
-        curr_error = loss_res.data[0][0]
+        curr_error = loss_res.item()
         iters += 1
     min_error = curr_error
-    params_max = torch.randn(x.shape[1], requires_grad=True)
+    params_max = torch.randn(x.shape[1], requires_grad=True,device="cuda")
     optim = Adam(params=[params_max], lr=0.05)
     iters = 0
     curr_error = 10000
     loss_max = loss_fn_generator(feature_num, minimize=True)
-    while (curr_error > 1) and (iters < 1000):
+    while (curr_error > 1) and (iters < 100):
         optim.zero_grad()
         loss_res = loss_max(params_max)
         loss_res.backward()
         optim.step()
-        curr_error = loss_res.data[0][0]
+        curr_error = loss_res.item()
         iters += 1
     max_error = curr_error
     if abs(max_error-initial_value) > abs(min_error-initial_value):
-        return min_error, torch.diag(sigmoid(x @ params_min)).data
-    return max_error, torch.diag(sigmoid(x @ params_min)).data
+        return min_error, torch.diag(sigmoid(x @ params_min)).cpu().detach().numpy()
+    return max_error, torch.diag(sigmoid(x @ params_min)).cpu().detach().numpy()
 
 
 def initial_value(feature_num):
     basis_list = [[0. for _ in range(x.shape[1])]]
     basis_list[0][feature_num] = 1.
-    basis = torch.tensor(basis_list, requires_grad=True)
-    x_t = torch.t(x)
-    denom = torch.inverse(x_t @ x)
-    return (basis @ (denom @ (x_t @ y)))
+    basis = torch.tensor(basis_list, requires_grad=True).cuda()
+    x_t = torch.t(x).cuda()
+    denom = torch.inverse(x_t @ x).cuda()
+    return (basis @ (denom @ (x_t @ y))).cuda()
 
 
 
@@ -72,8 +79,15 @@ def initial_value(feature_num):
 errors_and_weights = []
 for feature_num in range(x.shape[1]):
     full_dataset = initial_value(feature_num)
-    error, params = train_and_return(feature_num, full_dataset)
-
-    errors_and_weights.append((error, params, feature_num))
+    try:
+        error, _ = train_and_return(feature_num, full_dataset)
+        errors_and_weights.append((error,feature_num))
+        print(error, feature_num)
+    except RuntimeError as e:
+        print(e)
+        continue
 errors_sorted = sorted(errors_and_weights, key=lambda elem: abs(elem[0]), reverse=True)
 print(errors_sorted[0])
+print(compas_df.columns[errors_sorted[0][1]])
+
+
