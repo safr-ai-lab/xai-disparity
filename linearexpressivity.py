7d1bb9ccc677c4fbb1cabfcc02ff82b2bdd594df
pwchang <pchang534@gmail.com>
2022-11-11 12:38:42 -0500

    add intercept and remove final flats

--- linearexpressivity.py
+++ linearexpressivity.py
@@ -24,16 +24,17 @@ else:
     torch.device('cuda:0')
 
 
-def loss_fn_generator(x: torch.Tensor, y: torch.Tensor, initial_val: float, feature_num: int, sensitives: torch.Tensor):
+def loss_fn_generator(x_0: torch.Tensor, y: torch.Tensor, initial_val: float, feature_num: int, sensitives: torch.Tensor):
     """
     Factory for the loss function that pytorch runs will be optimizing in WLS
-    :param x: the data tensor
+    :param x_0: the data tensor with intercept column
     :param y: the target tensor
-    :param flat: The flat weight to add to each row -- prevents div by 0
     :param feature_num: Which feature in the data do we care about
     :param minimize: Boolean -- are we minimizing or maximizing
     :return: a loss function for our particular WLS problem.
     """
+    x = remove_intercept_column(x_0)
+
     basis_list = [[0. for _ in range(x.shape[1])]]
     basis_list[0][feature_num] = 1.
     flat_list = [0.001 for _ in range(x.shape[1])]
@@ -48,10 +49,10 @@ def loss_fn_generator(x: torch.Tensor, y: torch.Tensor, initial_val: float, feat
     # Look into derivation of gradient by hand and implementing it here instead.
     def loss_fn(params):
         # only train using sensitive features
-        one_d = sigmoid(x @ (sensitives * params))
-        # We add a flat value to prevent div by 0, then normalize by the trace
+        one_d = sigmoid(x_0 @ (sensitives * params))
         diag = torch.diag(one_d)
         x_t = torch.t(x)
+        # Try ridge reg by weight of params and not by flat value?
         denom = torch.inverse((x_t @ diag @ x) + torch.diag(flat))
         # we want to maximize this, so multiply by -1
         # remove subgroup size terminology (this optimizes subgroup size, is not ridge regularization)
@@ -76,7 +77,6 @@ def train_and_return(x: torch.Tensor, y: torch.Tensor, feature_num: int, initial
     niters = 1000
     # Set seed to const value for reproducibility
     torch.manual_seed(seed)
-
     s_list = [0. for _ in range(x.shape[1])]
     for f in f_sensitive:
         s_list[f] = 1.
@@ -98,7 +98,6 @@ def train_and_return(x: torch.Tensor, y: torch.Tensor, feature_num: int, initial
         optim.step()
         curr_error = loss_res.item()
         iters += 1
-    #print(params_max.grad)
     params_max = sensitives * params_max
     max_error = curr_error * -1
     assigns = (sigmoid(x @ params_max)).cpu().detach().numpy()
@@ -116,47 +115,41 @@ def initial_value(x: torch.Tensor, y: torch.Tensor, feature_num: int) -> float:
     """
     basis_list = [[0. for _ in range(x.shape[1])]]
     basis_list[0][feature_num] = 1.
-    flat_list = [0.001 for _ in range(x.shape[1])]
 
     if useCUDA:
         basis = torch.tensor(basis_list, requires_grad=True).cuda()
-        flat = torch.tensor(flat_list, requires_grad=True).cuda()
         x_t = torch.t(x).cuda()
-        denom = torch.inverse((x_t @ x) + torch.diag(flat)).cuda()
+        denom = torch.inverse(x_t @ x).cuda()
     else:
         basis = torch.tensor(basis_list, requires_grad=True)
-        flat = torch.tensor(flat_list, requires_grad=True)
         x_t = torch.t(x)
-        denom = torch.inverse((x_t @ x) + torch.diag(flat))
+        denom = torch.inverse(x_t @ x)
     return (basis @ (denom @ (x_t @ y))).item()
 
-def final_value(x: torch.Tensor, y: torch.Tensor, params: torch.Tensor, feature_num: int):
+def final_value(x_0: torch.Tensor, y: torch.Tensor, params: torch.Tensor, feature_num: int):
     """
     Given a defined subgroup function, returns the expressivity over the test data set
-    :param x: the test data tensor
+    :param x_0: the test data tensor
     :param y: the test target tensor
     :param params: tensor with coefficients defining the subgroup
     :param feature_num: the feature to test
     :return: the float value of expressivity over the dataset and subgroup assignments
     """
+    x = remove_intercept_column(x_0)
     basis_list = [[0. for _ in range(x.shape[1])]]
     basis_list[0][feature_num] = 1.
-    flat_list = [0.001 for _ in range(x.shape[1])]
 
     if useCUDA:
         basis = torch.tensor(basis_list, requires_grad=True).cuda()
-        flat = torch.tensor(flat_list, requires_grad=True).cuda()
         params = torch.tensor(params, requires_grad=True, device="cuda")
+        x_t = torch.t(x).cuda()
     else:
         basis = torch.tensor(basis_list, requires_grad=True)
-        flat = torch.tensor(flat_list, requires_grad=True)
+        x_t = torch.t(x)
 
-    one_d = sigmoid(x @ params)
-    # We add a flat value to prevent div by 0, then normalize by the trace
+    one_d = sigmoid(x_0 @ params)
     diag = torch.diag(one_d)
-    x_t = torch.t(x)
-    # TODO: does flat need to be added here?
-    denom = torch.inverse((x_t @ diag @ x) + torch.diag(flat))
+    denom = torch.inverse(x_t @ diag @ x)
     return (basis @ (denom @ (x_t @ diag @ y))).item(), one_d.cpu().detach().numpy()
 
 def find_extreme_subgroups(dataset: pd.DataFrame, seed: int, target_column: str, f_sensitive: list):
@@ -164,12 +157,12 @@ def find_extreme_subgroups(dataset: pd.DataFrame, seed: int, target_column: str,
     Given a dataset, finds the differential expressivity and maximal subset over all features.
     Saves that subset to a file.
     :param dataset: the pandas dataframe to use
-    :param seed: pseudorandom seed for reproducability
+    :param seed: pseudorandom seed for reproducibility
     :param target_column:  Which column in that dataframe is the target.
     :param f_sensitive: Which features are sensitive characteristics
     :return:  N/A.  Logs results.
     """
-    out_df = pd.DataFrame(columns = ['Feature', 'F(D)', 'max(F(S))', 'Difference', 'Subgroup Coefficients', 'Subgroup Size'])
+    out_df = pd.DataFrame()
 
     train_df, test_df = train_test_split(dataset, test_size=.2, random_state=seed)
 
@@ -184,14 +177,17 @@ def find_extreme_subgroups(dataset: pd.DataFrame, seed: int, target_column: str,
         y_test = torch.tensor(test_df[target_column].values).float()
         x_test = torch.tensor(test_df.drop(target_column, axis=1).values.astype('float16')).float()
     errors_and_weights = []
-    for feature_num in range(x_train.shape[1]):
-        print("Feature", feature_num, "of", x_train.shape[1])
-        total_exp_train = initial_value(x_train, y_train, feature_num)
+    for feature_num in range(x_train.shape[1]-1):
+        print("Feature", feature_num, "of", x_train.shape[1]-1)
+        x_train_ni = remove_intercept_column(x_train)
+        total_exp_train = initial_value(x_train_ni, y_train, feature_num)
         try:
             furthest_exp_train, assigns_train, params = train_and_return(x_train, y_train, feature_num, total_exp_train, f_sensitive, seed)
+            print(params)
             subgroup_size_train = [round(a) for a in assigns_train].count(1)/len(assigns_train)
             if not (np.isnan(furthest_exp_train)):
-                total_exp = initial_value(x_test, y_test, feature_num)
+                x_test_ni = remove_intercept_column(x_test)
+                total_exp = initial_value(x_test_ni, y_test, feature_num)
                 furthest_exp, assigns = final_value(x_test, y_test, params, feature_num)
                 subgroup_size = [round(a) for a in assigns].count(1) / len(assigns)
                 errors_and_weights.append((furthest_exp, feature_num))
@@ -223,18 +219,28 @@ def find_extreme_subgroups(dataset: pd.DataFrame, seed: int, target_column: str,
 
     return out_df
 
+def remove_intercept_column(x):
+    mask = torch.arange(0, x.shape[1]-1)
+    return torch.index_select(x, 1, mask)
 
 def run_system(df, target, sensitive_features, df_name, dummy=False):
     if dummy:
         df[target] = df[target].sample(frac=1).values
         df_name = 'dummy_' + df_name
 
+    # Add intercept column at the end
+    df['Intercept'] = np.ones(df.shape[0])
+
     # Sort columns so target is at end
     new_cols = [col for col in df.columns if col != target] + [target]
     df = df[new_cols]
 
-    # Get indices of sensitive features
+    # Get indices of sensitive features. Append a 1 for intercept
     f_sensitive = list(df.columns.get_indexer(sensitive_features))
+    f_sensitive.append(df.shape[1]-2)
+
+    print(df.shape[1])
+    print(f_sensitive)
 
     seeds = [0]
     for s in seeds:
