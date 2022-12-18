09c54c7ead592e0f739ea105920112d34d2d6be1
pwchang <pchang534@gmail.com>
2022-12-18 00:49:52 -0600

    fix script

--- process_LIME_exps.py
+++ process_LIME_exps.py
@@ -1,17 +1,24 @@
-from constrained_opt import split_out_dataset
+#from constrained_opt import split_out_dataset
 from lime_exp_func import LimeExpFunc
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from aif360.datasets import BankDataset
 import argparse
 import pandas as pd
 import json
 import time
 
+def split_out_dataset(dataset, target_column):
+    x = dataset.drop(target_column, axis=1).to_numpy()
+    y = dataset[target_column].to_numpy()
+    #sensitive_ds = dataset[f_sensitive].to_numpy()
+    return x, y
 
-parser = argparse.ArgumentParser(description='Process exps and save to file')
-parser.add_argument('seed', type=int)
-args = parser.parse_args()
-seed = args.seed
+
+# parser = argparse.ArgumentParser(description='Process exps and save to file')
+# parser.add_argument('seed', type=int)
+# args = parser.parse_args()
+seed = 0
 
 # df = pd.read_csv('data/student/student_cleaned.csv')
 # target = 'G3'
@@ -23,15 +30,15 @@ seed = args.seed
 # sensitive_features = ['age','sex_Male','race_African-American','race_Asian','race_Caucasian','race_Hispanic','race_Native American','race_Other']
 # df_name = 'compas_recid'
 
-df = pd.read_csv('data/compas/compas_cleaned_decile.csv')
-target = 'decile_score'
-sensitive_features = ['age','sex_Male','race_African-American','race_Asian','race_Caucasian','race_Hispanic','race_Native American','race_Other']
-df_name = 'compas_decile'
+# df = pd.read_csv('data/compas/compas_cleaned_decile.csv')
+# target = 'decile_score'
+# sensitive_features = ['age','sex_Male','race_African-American','race_Asian','race_Caucasian','race_Hispanic','race_Native American','race_Other']
+# df_name = 'compas_decile'
 
-# df = BankDataset().convert_to_dataframe()[0]
-# target = 'y'
-# sensitive_features = ['age', 'marital=married', 'marital=single', 'marital=divorced']
-# df_name = 'bank'
+df = BankDataset().convert_to_dataframe()[0]
+target = 'y'
+sensitive_features = ['age', 'marital=married', 'marital=single', 'marital=divorced']
+df_name = 'bank'
 
 # df = pd.read_csv('data/folktables/ACSIncome_MI_2018_new.csv')
 # target = 'PINCP'
@@ -39,11 +46,13 @@ df_name = 'compas_decile'
 #                       'RAC1P_3.0', 'RAC1P_4.0', 'RAC1P_5.0', 'RAC1P_6.0', 'RAC1P_7.0', 'RAC1P_8.0', 'RAC1P_9.0']
 # df_name = 'folktables'
 
+print('starting')
 new_cols = [col for col in df.columns if col != target] + [target]
 df = df[new_cols]
-train_df, test_df = train_test_split(df, test_size=0.5, random_state=seed)
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=seed)
 x_train, y_train = split_out_dataset(train_df, target)
 x_test, y_test = split_out_dataset(test_df, target)
+print('training classifier')
 classifier = RandomForestClassifier(random_state=seed)
 classifier.fit(x_train, y_train)
 
