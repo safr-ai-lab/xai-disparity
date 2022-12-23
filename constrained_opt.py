a26e6b4afc2fcc4c4393152a7fd2536937dcedf9
pwchang <pchang534@gmail.com>
2022-12-23 11:06:06 -0500

    cleaning up

--- constrained_opt.py
+++ constrained_opt.py
@@ -28,7 +28,6 @@ def argmin_g(x, y, feature_num, f_sensitive, exp_func, minimize, alphas):
     x_sensitive = x[:,f_sensitive]
     costs0 = [0 for _ in range(len(x))] # costs0 is always zeros
     learner = Learner(x_sensitive, y, linear_model.LinearRegression())
-    #learner = Learner(x, y, linear_model.LinearRegression())
 
     _ = 1
     while solver.v_t > v:
@@ -60,15 +59,12 @@ def argmin_g(x, y, feature_num, f_sensitive, exp_func, minimize, alphas):
 
         # lam^ <- avg(lambda), L_floor <- L(best_h(lam^), lam^)
         avg_lam = [np.mean(k) for k in zip(*solver.lambda_history)]
-        #print('avg_lam: ', avg_lam, ' | best_lam: ', best_lam, '| current lam: ', current_lam)
         best_g = solver.best_g(learner, feature_num, avg_lam, minimize)
         best_g_assigns, best_g_costs = best_g.predict(x_sensitive)
         best_g_exps = exp_func.get_total_exp(best_g_assigns, feature_num)
         L_floor = solver.lagrangian(best_g_assigns, avg_lam, feature_num, minimize)
 
         L = solver.lagrangian(avg_pred, avg_lam, feature_num, minimize)
-        #print(np.mean(assigns), np.mean(best_g_assigns), np.mean(avg_pred))
-        #print(L, L_floor, L_ceiling)
         solver.v_t = max(abs(L-L_floor), abs(L_ceiling-L))
 
         if solver.phi_s(assigns)+solver.phi_L(assigns) == 0:
@@ -111,7 +107,7 @@ def split_out_dataset(dataset, target_column):
     return x, y
 
 
-def extremize_exps_dataset(dataset, exp_func, target_column, f_sensitive, alphas, seed=0):
+def extremize_exps_dataset(dataset, exp_func, target_column, f_sensitive, alphas, seed=0, t_split=.5):
     np.random.seed(seed)
     """
     :param dataset: pandas dataframe
@@ -121,10 +117,6 @@ def extremize_exps_dataset(dataset, exp_func, target_column, f_sensitive, alphas
     :param seed: int, random seed
     :return: total expressivity over these rows
     """
-    if target_column == 'G3':
-        t_split = .5
-    else:
-        t_split = .2
     train_df, test_df = train_test_split(dataset, test_size=t_split, random_state=seed)
     x_train, y_train = split_out_dataset(train_df, target_column)
     x_test, y_test = split_out_dataset(test_df, target)
@@ -209,7 +201,7 @@ def extremize_exps_dataset(dataset, exp_func, target_column, f_sensitive, alphas
     return out_df
 
 
-def run_system(df, target, sensitive_features, df_name, dummy=False):
+def run_system(df, target, sensitive_features, df_name, dummy=False, t_split=.5):
     if dummy:
         df[target] = df[target].sample(frac=1).values
         df_name = 'dummy_' + df_name
@@ -226,7 +218,7 @@ def run_system(df, target, sensitive_features, df_name, dummy=False):
         print("Running", df_name, ", Alphas =", a)
         start = time.time()
         out = extremize_exps_dataset(dataset=df, exp_func=LimeExpFunc, target_column=target,
-                                     f_sensitive=f_sensitive, alphas=a)
+                                     f_sensitive=f_sensitive, alphas=a, t_split=t_split)
         final_df = pd.concat([final_df, out])
         print("Runtime:", '%.2f'%((time.time()-start)/3600), "Hours")
     date = datetime.today().strftime('%m_%d')
@@ -236,19 +228,30 @@ def run_system(df, target, sensitive_features, df_name, dummy=False):
 
 # df = pd.read_csv('data/student/student_cleaned.csv')
 # target = 'G3'
+# t_split = .5
 # sensitive_features = ['sex_M', 'Pstatus_T', 'address_U', 'Dalc', 'Walc', 'health']
 # # sensitive_features = list(df.columns).remove('G3')
 # df_name = 'student'
-# run_system(df, target, sensitive_features, df_name, dummy)
+# run_system(df, target, sensitive_features, df_name, dummy, t_split)
 
-# df = pd.read_csv('data/compas/compas_cleaned.csv')
-# target = 'two_year_recid'
-# sensitive_features = ['age','sex_Male','race_African-American','race_Asian','race_Caucasian','race_Hispanic','race_Native American','race_Other']
-# df_name = 'compas_recid'
-# run_system(df, target, sensitive_features, df_name, dummy)
-
-df = pd.read_csv('data/compas/compas_cleaned_decile.csv')
+df = pd.read_csv('data/compas/compas_decile_stripped.csv')
 target = 'decile_score'
+t_split = .5
 sensitive_features = ['age','sex_Male','race_African-American','race_Asian','race_Caucasian','race_Hispanic','race_Native American','race_Other']
-df_name = 'compas_decile'
-run_system(df, target, sensitive_features, df_name, dummy)
+df_name = 'stripped_compas_decile'
+run_system(df, target, sensitive_features, df_name, dummy, t_split)
+
+# df = BankDataset().convert_to_dataframe()[0]
+# target = 'y'
+# t_split = .2
+# sensitive_features = ['age', 'marital=married', 'marital=single', 'marital=divorced']
+# df_name = 'bank'
+# run_system(df, target, sensitive_features, df_name, dummy, t_split)
+
+# df = pd.read_csv('data/folktables/ACSIncome_MI_2018_new.csv')
+# target = 'PINCP'
+# t_split = .2
+# sensitive_features = ['AGEP', 'SEX', 'MAR_1.0', 'MAR_2.0', 'MAR_3.0', 'MAR_4.0', 'MAR_5.0', 'RAC1P_1.0', 'RAC1P_2.0',
+#                       'RAC1P_3.0', 'RAC1P_4.0', 'RAC1P_5.0', 'RAC1P_6.0', 'RAC1P_7.0', 'RAC1P_8.0', 'RAC1P_9.0']
+# df_name = 'folktables'
+# run_system(df, target, sensitive_features, df_name, dummy, t_split)
