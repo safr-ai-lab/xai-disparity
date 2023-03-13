df435fcd8e5264d6494cc02c5b0871da850b22a1
pwchang <pchang534@gmail.com>
2023-03-13 16:38:51 -0400

    improve user functionality

--- constrained_opt.py
+++ constrained_opt.py
@@ -1,32 +1,32 @@
 from helpers.constrained_solver import ConstrainedSolver
 from helpers.learner import Learner
 from sklearn import linear_model
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import time
-import json
 
 class SeparableSolver:
-    def __init__(self, df, target, sensitive_features, imp_method, df_name, t_split):
+    def __init__(self, df, trains, tests, imp_func_train, imp_func_test, f_sensitive, alphas, seed):
         self.df = df
-        self.target = target
-        self.sensitive_features = sensitive_features
-        self.imp_method = imp_method
-        self.df_name = df_name
-        self.t_split = t_split
+        self.x_train, self.y_train = trains[0], trains[1]
+        self.x_test, self.y_test = tests[0], tests[1]
+        self.imp_func_train = imp_func_train
+        self.imp_func_test = imp_func_test
+        self.f_sensitive = f_sensitive
+        self.alphas = alphas
+        np.random.seed(seed)
 
-    def argmin_g(self, x, y, feature_num, f_sensitive, imp_func, minimize, alphas):
-        imp_order = np.mean([abs(imp_func.imps[i][feature_num]) for i in range(len(x))])+.001
-        size_reg = len(x)*np.mean(alphas)
-        solver = ConstrainedSolver(imp_func, alpha_L=alphas[0], alpha_U=alphas[1], B=10000*imp_order, nu=.00001)
+    # Executes iterative CSC solve for a given feature
+    def argmin_g(self, feature_num, minimize):
+        imp_order = np.mean([abs(self.imp_func_train.imps[i][feature_num]) for i in range(len(self.x_train))])+.001
+        size_reg = len(self.x_train)*np.mean(self.alphas)
+        solver = ConstrainedSolver(self.imp_func_train, alpha_L=self.alphas[0], alpha_U=self.alphas[1], B=10000*imp_order, nu=.00001)
         v = .05*imp_order*size_reg
-        print('imp order:',imp_order, v)
+        print('imp order:', imp_order, v)
 
-        x_sensitive = x[:,f_sensitive]
-        costs0 = [0 for _ in range(len(x))] # costs0 is always zeros
-        learner = Learner(x_sensitive, y, linear_model.LinearRegression())
+        x_sensitive = self.x_train[:, self.f_sensitive]
+        costs0 = [0 for _ in range(len(self.x_train))] # costs0 is always zeros
+        learner = Learner(x_sensitive, self.y_train, linear_model.LinearRegression())
 
         _ = 1
         start2 = time.time()
@@ -37,14 +37,14 @@ class SeparableSolver:
             # h_t <- Best_h(lam_t)
             current_lam = solver.lambda_history[-1]
             if minimize:
-                costs1 = [imp_func.imps[i][feature_num]-current_lam[0]+current_lam[1] for i in range(len(x))]
+                costs1 = [self.imp_func_train.imps[i][feature_num]-current_lam[0]+current_lam[1] for i in range(len(self.x_train))]
             else:
-                costs1 = [-imp_func.imps[i][feature_num]-current_lam[0]+current_lam[1] for i in range(len(x))]
+                costs1 = [-self.imp_func_train.imps[i][feature_num]-current_lam[0]+current_lam[1] for i in range(len(self.x_train))]
             l_response = learner.best_response(costs0, costs1)
             solver.g_history.append(l_response)
 
             assigns, cost = l_response.predict(x_sensitive)
-            importance = imp_func.get_total_imp(assigns, feature_num)
+            importance = self.imp_func_train.get_total_imp(assigns, feature_num)
             solver.pred_history.append(np.array(assigns))
             solver.size_history.append(np.mean(assigns))
             solver.imp_history.append(importance)
@@ -79,61 +79,20 @@ class SeparableSolver:
             total += row[feature_num]
         return total
 
-    def split_out_dataset(self, dataset, target_column):
-        x = dataset.drop(target_column, axis=1).to_numpy()
-        y = dataset[target_column].to_numpy()
-        #sensitive_ds = dataset[f_sensitive].to_numpy()
-        return x, y
-
-
-    def extremize_imps_dataset(self, dataset, imp_func, target_column, f_sensitive, alphas, seed=0, t_split=.5):
-        np.random.seed(seed)
-        """
-        :param dataset: pandas dataframe
-        :param imp_func: class for impressivities
-        :param target_column: string, column name in dataset
-        :param f_sensitive: list of column names that are sensitive features
-        :param seed: int, random seed
-        :return: total importance over these rows
-        """
-        train_df, test_df = train_test_split(dataset, test_size=t_split, random_state=seed)
-        x_train, y_train = self.split_out_dataset(train_df, target_column)
-        x_test, y_test = self.split_out_dataset(test_df, target_column)
-        classifier = RandomForestClassifier(random_state=seed)
-        classifier.fit(x_train, y_train)
+    # Iterate through all features, find min/max subgroups, choose largest FID subgroup
+    def extremize_imps_dataset(self):
         out_df = pd.DataFrame()
-
-        imp_func_train = imp_func(classifier, x_train, y_train, seed)
-        #print("Populating train importance values")
-        #imp_func_train.populate_imps()
-
-        imp_func_test = imp_func(classifier, x_test, y_test, seed)
-        #print("Populating test importance values")
-        #imp_func_test.populate_imps()
-
-        # Populating with pre-computed values
-        with open(f'input/imps/{self.df_name}_train_{self.imp_method}_seed0', 'r') as f:
-            train_temp = list(map(json.loads, f))[0]
-        for e_list in train_temp:
-            imp_func_train.imps.append({int(k):v for k,v in e_list.items()})
-        with open(f'input/imps/{self.df_name}_test_{self.imp_method}_seed0', 'r') as f:
-            test_temp = list(map(json.loads, f))[0]
-        for e_list in test_temp:
-            imp_func_test.imps.append({int(k):v for k,v in e_list.items()})
-
-        #for feature_num in range(len(x_train[0])):
-        for feature_num in range(1):
+        for feature_num in range(len(self.x_train[0])):
             print('*****************')
-            print(train_df.columns[feature_num])
-            total_imp_train = self.full_dataset_importance(imp_func_train, feature_num)
+            print(self.df.columns[feature_num])
+            total_imp_train = self.full_dataset_importance(self.imp_func_train, feature_num)
             print('total imp: ', total_imp_train)
-            min_solver = self.argmin_g(x_train, y_train, feature_num, f_sensitive, imp_func_train,
-                                                       minimize=True, alphas=alphas)
+
+            min_solver = self.argmin_g(feature_num, minimize=True)
             min_model, min_assigns, min_imp = min_solver.get_best_valid_model(minimize=True)
             print('min imp', min_imp, '| size', np.mean(min_assigns))
 
-            max_solver = self.argmin_g(x_train, y_train, feature_num, f_sensitive, imp_func_train,
-                                                       minimize=False, alphas=alphas)
+            max_solver = self.argmin_g(feature_num, minimize=False)
             max_model, max_assigns, max_imp = max_solver.get_best_valid_model(minimize=False)
             print('max imp', max_imp, '| size', np.mean(max_assigns))
 
@@ -153,23 +112,23 @@ class SeparableSolver:
             subgroup_size_train = np.mean(assigns_train)
 
             # compute test values
-            total_imp_test = self.full_dataset_importance(imp_func_test, feature_num)
+            total_imp_test = self.full_dataset_importance(self.imp_func_test, feature_num)
             #assigns_test = get_avg_prediction(best_model, x_test) # mix model method
-            assigns_test = best_model.predict(x_test[:,f_sensitive])[0] # sensitive features only method
+            assigns_test = best_model.predict(self.x_test[:, self.f_sensitive])[0] # sensitive features only method
             #assigns_test = best_model.predict(x_test)[0]
             subgroup_size_test = np.mean(assigns_test)
             furthest_imp_test = 0
             for i in range(len(assigns_test)):
-                furthest_imp_test += assigns_test[i]*imp_func_test.imps[i][feature_num]
+                furthest_imp_test += assigns_test[i]*self.imp_func_test.imps[i][feature_num]
 
             # # from mix models, pick model with largest imp diff that is valid
             params = best_model.b1.coef_
-            params_with_labels = {dataset.columns[i]: float(param) for (i, param) in zip(f_sensitive, params)}
+            params_with_labels = {self.df.columns[i]: float(param) for (i, param) in zip(self.f_sensitive, params)}
             params_with_labels['Intercept'] = best_model.b1.intercept_
             print(params_with_labels)
             out_df = pd.concat([out_df,
-                                pd.DataFrame.from_records([{'Feature': dataset.columns[feature_num],
-                                                            'Alpha': alphas,
+                                pd.DataFrame.from_records([{'Feature': self.df.columns[feature_num],
+                                                            'Alpha': self.alphas,
                                                             'F(D)': total_imp_test,
                                                             'max(F(S))': furthest_imp_test,
                                                             'Difference': abs(furthest_imp_test - total_imp_test),
--- run_separable.py
+++ run_separable.py
@@ -6,12 +6,24 @@ import time
 import argparse
 from datetime import datetime
 import sys
+import json
 from constrained_opt import SeparableSolver
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+
+def split_out_dataset(dataset, target_column):
+    x = dataset.drop(target_column, axis=1).to_numpy()
+    y = dataset[target_column].to_numpy()
+    return x, y
+
 
 parser = argparse.ArgumentParser(description='Locally separable run')
-parser.add_argument('imp_method', type=str)
+parser.add_argument('--method', type=str, required=True)
+parser.add_argument('--file', type=str, required=False)
 args = parser.parse_args()
-imp_method = args.imp_method
+imp_method = args.method
+imp_file = args.file
 
 if imp_method == 'lime':
     impFunc = LimeImpFunc
@@ -22,24 +34,62 @@ elif imp_method == 'grad':
 else:
     sys.exit('Exp method not recognized')
 
+
+# User: Define dataset here
 df = pd.read_csv('data/student/student_cleaned.csv')
 target = 'G3'
 t_split = .5
 sensitive_features = ['sex_M', 'Pstatus_T', 'address_U', 'Dalc', 'Walc', 'health']
 df_name = 'student'
 
-# Sort columns so target is at end
+# Sorting columns so target is at end
 new_cols = [col for col in df.columns if col != target] + [target]
 df = df[new_cols]
-
 f_sensitive = list(df.columns.get_indexer(sensitive_features))
 
+# User: Define classifier here
+seed = 0
+train_df, test_df = train_test_split(df, test_size=t_split, random_state=seed)
+x_train, y_train = split_out_dataset(train_df, target)
+x_test, y_test = split_out_dataset(test_df, target)
+classifier = RandomForestClassifier(random_state=seed)
+classifier.fit(x_train, y_train)
+
+# User: Define alpha range here
 a = [.01, .05]
+
+
+# Read in importances file if specified, else compute importances
+imp_func_train = impFunc(classifier, x_train, y_train, seed)
+imp_func_test = impFunc(classifier, x_test, y_test, seed)
+
+try:
+    with open(f'{imp_file}_train', 'r') as f:
+        train_temp = list(map(json.loads, f))[0]
+    for e_list in train_temp:
+        imp_func_train.imps.append({int(k): v for k, v in e_list.items()})
+    with open(f'{imp_file}_test', 'r') as f:
+        test_temp = list(map(json.loads, f))[0]
+    for e_list in test_temp:
+        imp_func_test.imps.append({int(k): v for k, v in e_list.items()})
+except:
+    print("Importance file not specified or found...")
+    print("Populating train importance values")
+    imp_func_train.populate_imps()
+    with open(f'input/imps/{df_name}_{imp_method}_seed{seed}_train', 'w') as fout:
+        json.dump(imp_func_train.imps, fout)
+
+    print("Populating test importance values")
+    imp_func_test.populate_imps()
+    with open(f'input/imps/{df_name}_{imp_method}_seed{seed}_test', 'w') as fout:
+        json.dump(imp_func_test.imps, fout)
+
+
 print("Running", df_name, ", Alphas =", a)
 start = time.time()
-solver = SeparableSolver(df, target, sensitive_features, imp_method, df_name, t_split)
-final_df = solver.extremize_imps_dataset(dataset=df, imp_func=impFunc, target_column=target,
-                                  f_sensitive=f_sensitive, alphas=a, t_split=t_split)
+solver = SeparableSolver(df, (x_train, y_train), (x_test, y_test), imp_func_train, imp_func_test,
+                         f_sensitive, a, seed)
+final_df = solver.extremize_imps_dataset()
 print("Runtime:", '%.2f' % ((time.time() - start) / 3600), "Hours")
 date = datetime.today().strftime('%m_%d')
 final_df.to_csv(f'output/{df_name}_{imp_method}_output_{date}_alpha{a}.csv')
